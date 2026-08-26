69bdc5c7305204995d558e0e4daa769502abd572
Panakj Patil <patilpandurang1164@gmail.com>
2026-08-26 15:58:35 +0530

    Assignment Operators

---
+++ Assignment.c
@@ -0,0 +1,19 @@
+//Working of assignment operaturs
+#include<stdio.h>
+int main()
+{
+    int a=5, c;
+    c=a;    //c is 5
+    printf("c = %d\n", c);
+    c+=a;
+    printf("c = %d\n", c);
+    c-=a;
+    printf("c = %d\n", c);
+    c*=a;
+    printf("c = %d\n", c);
+    c/=a;
+    printf("c = %d\n", c);
+    c%=a;
+    printf("c = %d\n", c);
+    return 0;
+}
